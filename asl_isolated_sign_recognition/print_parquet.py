e88ec64eec002450d21e1c434054e4b7e723e797
Andreas Rugaard Klaesoee <andreas_klaesoe@apple.com>
2023-10-25 13:11:44 +0200

    adds extra data parsing for parquet files

--- asl_isolated_sign_recognition/print_parquet.py
+++ asl_isolated_sign_recognition/print_parquet.py
@@ -1,7 +1,18 @@
 import pandas
+import os
 
-parquet_file = "data/asl-signs/train_landmark_files/2044/635217.parquet"
+asl_signs_data = "data/asl-signs"
+asl_signs_landmark_files = os.path.join(asl_signs_data, 'train_landmark_files')
+participants = os.listdir(asl_signs_landmark_files)
+signs_per_participant = {participant : os.listdir(os.path.join(asl_signs_landmark_files, participant)) for participant in participants}
 
-parquet_contents = pandas.read_parquet(parquet_file)
+for participant, parquet_files in signs_per_participant.items():
+    for parquet in parquet_files:
+        parquet_file = pandas.read_parquet(os.path.join(asl_signs_landmark_files, participant, parquet))
+        unique = pandas.unique(parquet_file['frame'])
+    break
 
-print(parquet_contents)
+print(parquet_file)
+print(unique)
+
+print(3258 / 6)
